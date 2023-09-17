07ac5b06e8e1d87075e295d54fb0ddfc3c5aec1c
Domino731 <d.orzechowski18@gmail.com>
2023-09-17 17:48:32 +0200

    feature - hero - title

--- src/modules/cyberpunk/hero/CpHero.tsx
+++ src/modules/cyberpunk/hero/CpHero.tsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss';
 
 export const CpHero = () => {
     return <div className={styles.container}>
-
+        {/*TODO: ADD AI IMAGE */}
+          <h1 className={styles.title}>FULL-STACK DEVELOPER</h1>
     </div>
 }
