03e9dd91b162ad6b4780f55ef3a69b98f4ecef97
Domino731 <d.orzechowski18@gmail.com>
2023-09-17 17:17:23 +0200

    feature - cyberpunk header - last styles

--- src/modules/cyberpunk/header/CpHeader.tsx
+++ src/modules/cyberpunk/header/CpHeader.tsx
@@ -2,18 +2,31 @@ import styles from './styles.module.scss'; // Import the SCSS modul
 import logo from "../../../assets/cyberpunk/images/logo.png";
 import {headerOptions} from "../../app/sections/Header";
 import {HeaderOption} from "../../app/sections/Header/header.types.ts";
+import {useMemo} from "react";
 
 export const CpHeader = () => {
+
+    const options = useMemo(() => {
+       const data = [...headerOptions];
+        data.pop();
+        data.shift();
+
+        return data;
+    }, []);
+
     return <div className={styles.container}>
            <img src={logo}/>
-        <nav>
+         <nav>
             <ul className={styles.list}>
-                {headerOptions.map(({label}: HeaderOption) => <li className={styles.listItem}>
+                <li className={styles.listItem}>
+                    <a className={`${styles.link} ${styles.linkFirstItem}`}>{headerOptions[0].label}</a>
+                </li>
+                {options.map(({label}: HeaderOption) => <li className={styles.listItem}>
                     <a className={styles.link}>{label}</a>
                 </li>)}
 
                 <li className={styles.listItem}>
-                    <a className={styles.link}>CONTACT</a>
+                    <a className={`${styles.link} ${styles.linkLastItem}`}>{headerOptions[headerOptions.length - 1].label}</a>
                 </li>
             </ul>
         </nav>
