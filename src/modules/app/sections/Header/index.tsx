43ba999783bed2a1d2378729625dabb16c89af44
Domino731 <d.orzechowski18@gmail.com>
2023-09-17 12:33:32 +0200

    feature - cyberpunk header - basic header

--- src/modules/app/sections/Header/index.tsx
+++ src/modules/app/sections/Header/index.tsx
@@ -1,3 +1,4 @@
 import {Header} from "./Header.tsx";
+import {headerOptions} from "./header.const.ts";
 
-export {Header}
+export {Header, headerOptions}
--- src/modules/cyberpunk/header/CpHeader.tsx
+++ src/modules/cyberpunk/header/CpHeader.tsx
@@ -1,8 +1,21 @@
 import styles from './styles.module.scss'; // Import the SCSS modul
 import logo from "../../../assets/cyberpunk/images/logo.png";
+import {headerOptions} from "../../app/sections/Header";
+import {HeaderOption} from "../../app/sections/Header/header.types.ts";
 
 export const CpHeader = () => {
     return <div className={styles.container}>
            <img src={logo}/>
+        <nav>
+            <ul className={styles.list}>
+                {headerOptions.map(({label}: HeaderOption) => <li className={styles.listItem}>
+                    <a className={styles.link}>{label}</a>
+                </li>)}
+
+                <li className={styles.listItem}>
+                    <a className={styles.link}>CONTACT</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 }
