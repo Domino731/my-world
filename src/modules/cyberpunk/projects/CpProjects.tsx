a5802d4d2ebe858c57f2eda8c3677c5124bf6541
Domino731 <d.orzechowski18@gmail.com>
2023-09-21 22:18:49 +0200

    feature - cp projects - own project box

--- src/modules/cyberpunk/projects/CpProjects.tsx
+++ src/modules/cyberpunk/projects/CpProjects.tsx
@@ -14,6 +14,7 @@ export const CpProjects = () => {
         <SubTitle title="The most important part of being a programmer. I put my heart into it, and user experience at first place."/>
         <div className={styles.content}>
             {/* COMMERCIAL PROJECTS */}
+            <div className={styles.commercialProjects}>
             <div className={styles.projectsList}>
                 <h3 className={`${styles.projectsListTitle} ${styles.projectsListTitleCommercial}`}>
                     COMMERCIAL
@@ -33,24 +34,26 @@ export const CpProjects = () => {
                     </li>)}
                 </ul>
             </div>
-
+            </div>
             {/* OWN PROJECTS */}
-            <div className={styles.projectsList}>
-                <h3 className={`${styles.projectsListTitle} ${styles.projectsListTitleOwn}`}>
-                    OWN
-                </h3>
-                <ul className={`${styles.list} ${styles.projectsListCommercial}`}>
-                    {commercialProjects.map(({title, description, linkTitle}) =>                     <li className={styles.projectsListItem}>
-                        <div className={styles.projectsListItemContent}>
-                            <DataHeader title="_PROJECT_TITLE"/>
-                            <p className={`${styles.projectText} ${styles.projectTitle}`}>{title}</p>
-                            <DataHeader title="_PROJECT_DESCRIPTION"/>
-                            <p className={`${styles.projectText} ${styles.projectDescription}`}> {description}</p>
-                            <DataHeader title="_PROJECT_LINK"/>
-                            <p className={`${styles.projectText} ${styles.projectDescription}`}>{linkTitle}</p>
-                        </div>
-                    </li>)}
-                </ul>
+            <div className={styles.ownProjects}>
+                <div className={styles.projectsList}>
+                    <h3 className={`${styles.projectsListTitle} ${styles.projectsListTitleOwn}`}>
+                        OWN
+                    </h3>
+                    <ul className={`${styles.list} ${styles.projectsListCommercial}`}>
+                        {commercialProjects.map(({title, description, linkTitle}) => <li className={`${styles.projectsListItem} ${styles.projectsListItemOwn}`}>
+                            <div className={styles.projectsListItemContent}>
+                                <DataHeader title="_PROJECT_TITLE"/>
+                                <p className={`${styles.projectText} ${styles.projectTitle}`}>{title}</p>
+                                <DataHeader title="_PROJECT_DESCRIPTION"/>
+                                <p className={`${styles.projectText} ${styles.projectDescription}`}> {description}</p>
+                                <DataHeader title="_PROJECT_LINK"/>
+                                <p className={`${styles.projectText} ${styles.projectDescription} ${styles.ownProjectLink}`}>{linkTitle}</p>
+                            </div>
+                        </li>)}
+                    </ul>
+                </div>
             </div>
         </div>
     </section>
