597b32bcc2d85fca7471e19673825c9513d289d9
Domino731 <d.orzechowski18@gmail.com>
2023-09-21 22:03:14 +0200

    feature - cp projects - polygon for own projects title

--- src/modules/cyberpunk/projects/CpProjects.tsx
+++ src/modules/cyberpunk/projects/CpProjects.tsx
@@ -13,11 +13,31 @@ export const CpProjects = () => {
         <Title title="PROJECTS"/>
         <SubTitle title="The most important part of being a programmer. I put my heart into it, and user experience at first place."/>
         <div className={styles.content}>
+            {/* COMMERCIAL PROJECTS */}
             <div className={styles.projectsList}>
                 <h3 className={`${styles.projectsListTitle} ${styles.projectsListTitleCommercial}`}>
                     COMMERCIAL
                     <span/>
                     <span/>
+                </h3>
+                <ul className={`${styles.list} ${styles.projectsListCommercial}`}>
+                    {commercialProjects.map(({title, description, linkTitle}) =>                     <li className={styles.projectsListItem}>
+                        <div className={styles.projectsListItemContent}>
+                            <DataHeader title="_PROJECT_TITLE"/>
+                            <p className={`${styles.projectText} ${styles.projectTitle}`}>{title}</p>
+                            <DataHeader title="_PROJECT_DESCRIPTION"/>
+                            <p className={`${styles.projectText} ${styles.projectDescription}`}> {description}</p>
+                            <DataHeader title="_PROJECT_LINK"/>
+                            <p className={`${styles.projectText} ${styles.projectDescription}`}>{linkTitle}</p>
+                        </div>
+                    </li>)}
+                </ul>
+            </div>
+
+            {/* OWN PROJECTS */}
+            <div className={styles.projectsList}>
+                <h3 className={`${styles.projectsListTitle} ${styles.projectsListTitleOwn}`}>
+                    OWN
                 </h3>
                 <ul className={`${styles.list} ${styles.projectsListCommercial}`}>
                     {commercialProjects.map(({title, description, linkTitle}) =>                     <li className={styles.projectsListItem}>
