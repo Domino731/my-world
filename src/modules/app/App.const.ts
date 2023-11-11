30e46b2d54cbd6db1355bb6ec483ec6599766e22
Domino731 <d.orzechowski18@gmail.com>
2023-11-11 19:42:37 +0100

    tests - jest added to project

--- src/modules/app/App.const.ts
+++ src/modules/app/App.const.ts
@@ -2,7 +2,8 @@ export enum APP_SECTION_NAMES {
     CYBERPUNK = "CYBERPUNK",
     MODERN = "MODERN",
     BORING = "BORING",
-    ERROR = "ERROR"
+    ERROR = "ERROR",
+    PIXEL = "PIXEL"
 }
 
 export type AppSectionNamesUnion = keyof typeof APP_SECTION_NAMES;
@@ -11,5 +12,6 @@ export const APP_SECTIONS_ORDER: Array<AppSectionNamesUnion> = [
     APP_SECTION_NAMES.CYBERPUNK,
     APP_SECTION_NAMES.MODERN,
     APP_SECTION_NAMES.BORING,
-    APP_SECTION_NAMES.ERROR
+    APP_SECTION_NAMES.ERROR,
+    APP_SECTION_NAMES.PIXEL
 ];
