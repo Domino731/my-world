85b8a0df5d67ae1ebe0f7eb22e2f9821c31d0c89
Domino731 <d.orzechowski18@gmail.com>
2023-11-11 19:32:04 +0100

    feature - app - title for switch button

--- src/modules/app/SwitchStyle/SwitchButton/SwitchButton.tsx
+++ src/modules/app/SwitchStyle/SwitchButton/SwitchButton.tsx
@@ -2,10 +2,11 @@ import styles from "./styles.module.scss";
 import {SwitchButtonProps} from "./SwitchButton.types.ts";
 import * as classNames from "classnames";
 import {APP_SECTIONS} from "../../App.types.ts";
+import {useMemo} from "react";
 
 export const SwitchButton = ({isRight, appSection}: SwitchButtonProps) => {
 
-    const isCp =  appSection === APP_SECTIONS.CP
+    const isCp = appSection === APP_SECTIONS.CP
 
     const className = classNames(styles.button, {
         // GENERAL STYLES
@@ -15,10 +16,15 @@ export const SwitchButton = ({isRight, appSection}: SwitchButtonProps) => {
         [styles.cpButton]: isCp,
         [styles.cpLeft]: !isRight && isCp,
         [styles.cpRight]: isRight && isCp,
-    })
+    });
 
-    return <button className={className}>
-        {isCp && !isRight && <i className="fa-solid fa-chevron-left"></i> }
-        {isCp && isRight && <i className="fa-solid fa-chevron-right"></i> }
+    const title = useMemo(() => {
+        if (isRight) return "Switch to next section";
+        return "Switch to previous section";
+    }, [isRight])
+
+    return <button className={className} title={title}>
+        {isCp && !isRight && <i className="fa-solid fa-chevron-left"></i>}
+        {isCp && isRight && <i className="fa-solid fa-chevron-right"></i>}
     </button>
 }
