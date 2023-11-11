d3b5465b5585e26d2db616dfc4c18855c82d9f37
Domino731 <d.orzechowski18@gmail.com>
2023-11-11 19:49:07 +0100

    test

--- src/modules/app/app.test.js
+++ src/modules/app/app.test.js
@@ -1,3 +1,15 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(4);
-});
+import {APP_SECTION_NAMES, APP_SECTIONS_ORDER} from "./App.const.js";
+
+describe('Check if APP_SECTIONS_ORDER array contains all items from APP_SECTION_NAMES enum ', () => {
+    it('contains all items from APP_SECTION_NAMES enum', () => {
+        const enumValues = Object.values(APP_SECTION_NAMES);
+
+        // Check if each enum value is present in APP_SECTIONS_ORDER
+        enumValues.forEach((enumValue) => {
+            expect(APP_SECTIONS_ORDER).toContain(enumValue);
+        });
+
+        // Check if APP_SECTIONS_ORDER has the same length as the enum
+        expect(APP_SECTIONS_ORDER).toHaveLength(enumValues.length);
+    });
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {createTsJestConfig} from './jest.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    ...createTsJestConfig(),
 })
